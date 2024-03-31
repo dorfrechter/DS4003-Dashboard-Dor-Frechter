9da93290001624c187fc6b646cfee657331cd08b
dor.frechter <dor.frechter@gmail.com>
2024-03-30 20:32:36 -0700

    d

---
+++ app.py
@@ -0,0 +1,6 @@
+# import dependencies
+
+from dash import Dash, html, dcc
+from dash.dependencies import Input, Output  # Make sure to import Input and Output
+import pandas as pd
+import plotly.express as px
